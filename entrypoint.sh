f7509311c0e35069bbab533bae059f446fe7a073
Laurence8866 <90469915+Laurence8866@users.noreply.github.com>
2021-09-19 12:57:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-sed 's/rpc-secret.*/rpc-secret='$rpc'/g' -i   /root/.aria2/aria2.conf
+sed 's/rpc-secret.*/rpc-secret=123456/g' -i   /root/.aria2/aria2.conf
 filebrowser -r /  --baseurl /file &
 /home/caddy/caddy --conf=/home/caddy/Caddyfile -agree  &
 /etc/init.d/aria2 start
